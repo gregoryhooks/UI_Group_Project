a0d6155b2a4b2070a5f783371065264480842d66
gregoryhooks <gregoryhooksjr@gmail.com>
2015-11-12 16:28:10 -0600

    Added different types of part queries for compatibility purposes.  Added example usages in PartsDB_tests.php.

--- WebContent/models/PartsDB.class.php
+++ WebContent/models/PartsDB.class.php
@@ -16,7 +16,7 @@ class PartsDB {
 			//$query = $query." = :$id)";
 			
 			//$query = "SELECT * FROM harddrives WHERE (hdriveId = '1hdd05')";
-			$query = "SELECT * FROM ".$table." WHERE (".$typeid." = '".$id."')";
+			$query = "SELECT * FROM ".$table." WHERE (`".$typeid."` = '".$id."')";
 				
 			$statement = $db->prepare($query);
 			//$statement->bindParam(":$table", $table);
@@ -30,6 +30,113 @@ class PartsDB {
 			echo "<p>Error getting part rows by $type: " . $e->getMessage () . "</p>";
 		}
 		return $partRowSets;
+	}
+	
+	public static function getPartRowSetsByLowerPrice($table = null, $typeid = null, $id = null) {
+		// Returns the rows of Parts whose $type field has value $value
+		$partRowSets = NULL;
+		try {
+			$db = Database::getDB ();
+			//$query = "SELECT * FROM :$table WHERE (";//= :$id)";
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = $id)";
+			//"harddrives", "hdriveId", "1hdd05"
+	
+			//if (strcmp($typeid, "hdriveId") == 0)
+			//	$query = $query."hdriveId";
+				
+			//$query = $query." = :$id)";
+				
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = '1hdd05')";
+			$query = "SELECT * FROM ".$table." WHERE (".$typeid." <= '".$id."')";
+	
+			$statement = $db->prepare($query);
+			//$statement->bindParam(":$table", $table);
+			//$statement->bindParam(":$typeid", $typeid);
+			//$statement->bindParam(":$id", $id);
+			$statement->execute ();
+				
+			$partRowSets = $statement->fetchAll(PDO::FETCH_ASSOC);
+			$statement->closeCursor ();
+		} catch (Exception $e) { // Not permanent error handling
+			echo "<p>Error getting part rows by $type: " . $e->getMessage () . "</p>";
+		}
+		return $partRowSets;
+	}
+	
+	public static function getCPUPartRowSetsByMotherboard($id = null) {
+		if (substr($id, -1) == 'I') {
+			$brand = "Intel";
+		} else {
+			$brand = "AMD";
+		}
+		// Returns the rows of Parts whose $type field has value $value
+		$partRowSets = NULL;
+		try {
+			$db = Database::getDB ();
+			//$query = "SELECT * FROM :$table WHERE (";//= :$id)";
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = $id)";
+			//"harddrives", "hdriveId", "1hdd05"
+	
+			//if (strcmp($typeid, "hdriveId") == 0)
+			//	$query = $query."hdriveId";
+				
+			//$query = $query." = :$id)";
+				
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = '1hdd05')";
+			$query = "SELECT * FROM cpus WHERE (`Make` = '".$brand."')";
+	
+			$statement = $db->prepare($query);
+			//$statement->bindParam(":$table", $table);
+			//$statement->bindParam(":$typeid", $typeid);
+			//$statement->bindParam(":$id", $id);
+			$statement->execute ();
+				
+			$partRowSets = $statement->fetchAll(PDO::FETCH_ASSOC);
+			$statement->closeCursor ();
+		} catch (Exception $e) { // Not permanent error handling
+			echo "<p>Error getting part rows by $type: " . $e->getMessage () . "</p>";
+		}
+		
+		return $partRowSets;
+	}
+	
+	public static function getMotherboardPartRowSetsByCPU($id = null) {
+		// Returns the rows of Parts whose $type field has value $value
+		$partRowSets = NULL;
+		try {
+			$db = Database::getDB ();
+			//$query = "SELECT * FROM :$table WHERE (";//= :$id)";
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = $id)";
+			//"harddrives", "hdriveId", "1hdd05"
+	
+			//if (strcmp($typeid, "hdriveId") == 0)
+			//	$query = $query."hdriveId";
+	
+			//$query = $query." = :$id)";
+	
+			//$query = "SELECT * FROM harddrives WHERE (hdriveId = '1hdd05')";
+			$query = "SELECT * FROM motherboards";
+	
+			$statement = $db->prepare($query);
+			//$statement->bindParam(":$table", $table);
+			//$statement->bindParam(":$typeid", $typeid);
+			//$statement->bindParam(":$id", $id);
+			$statement->execute ();
+	
+			$partRowSets = $statement->fetchAll(PDO::FETCH_ASSOC);
+			$statement->closeCursor ();
+		} catch (Exception $e) { // Not permanent error handling
+			echo "<p>Error getting part rows by $type: " . $e->getMessage () . "</p>";
+		}
+	
+		$motherboards = array();
+		foreach ($partRowSets as $part){
+			if (substr($part['mboardId'], -1) == substr($id, -3, -2)) {
+				array_push($motherboards, $part);
+			}
+		}
+		
+		return $motherboards;
 	}
 	
 }
--- WebContent/tests/PartsDB_tests.php
+++ WebContent/tests/PartsDB_tests.php
@@ -24,5 +24,88 @@ foreach ($parts as $part) {
 }
 ?>
 
+
+
+<h2>Query HardDrive Size By ID</h2>
+<?php
+$parts = PartsDB::getPartRowSetsBy("harddrives", "hdriveId", "1hdd05");
+echo "The hard drive size with id \"1hdd05\" is: ";
+echo $parts[0]['Size'];
+echo "<br>";
+?>
+
+<h2>Query HardDrives By Size</h2>
+<?php
+$parts = PartsDB::getPartRowSetsBy("harddrives", "Size", "250GB");
+echo "The hard drives with size \"250GB\" are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query Motherboards By Number of Memory Slots</h2>
+<?php
+$parts = PartsDB::getPartRowSetsBy("motherboards", "Memory Slots", "4");
+echo "The motherboards with \"4\" slots are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query Motherboards By Memory Type</h2>
+<?php
+$parts = PartsDB::getPartRowSetsBy("motherboards", "Memory Type", "DDR4");
+echo "The motherboards with Memory Type \"DDR4\" are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query Motherboards By Number of USB Ports</h2>
+<?php
+$parts = PartsDB::getPartRowSetsBy("motherboards", "USB Ports", "8");
+echo "The motherboards with \"8\" USB Ports are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query GPUs Less than a Given Price</h2>
+<p>Works for any given part, you might have to change "price" to "Price" depending on the database column captialization (blame Eric)</p>
+<?php
+$parts = PartsDB::getPartRowSetsByLowerPrice("gpus", "price", "200.00");
+echo "The GPUs with a price less than \"$200\" are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query CPUs the are Compatible with a Given Motherboard ID</h2>
+<?php
+$parts = PartsDB::getCPUPartRowSetsByMotherboard("8mb09I");
+echo "The CPUs compatible with the motherboard with ID \"8mb09I\" are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+<h2>Query Motherboards the are Compatible with a Given CPU ID</h2>
+<?php
+$parts = PartsDB::getMotherboardPartRowSetsByCPU("1cpuI08");
+echo "The motherboards compatible with the CPU with ID \"1cpuI08\" are: <br>";
+foreach ($parts as $part){
+	print_r($part);
+	echo "<br>";
+}
+?>
+
+
+
 </body>
 </html>
