4ac5917d4b8d086de601aa1ccdb0a00c2f43b539
bjv500 <eapplonie@hotmail.com>
2015-12-07 12:21:25 -0600

    login works for real now

--- WebContent/controllers/LoginController.class.php
+++ WebContent/controllers/LoginController.class.php
@@ -1,39 +1,24 @@
 <?php
 class LoginController {
-
-	/*public static function run() {
+	public static function run() {
+		
+	
+		$user = null;
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$user = new User($_POST);  
-			if ($user->getErrorCount() == 0) 
-				homeView::show($user);
-		    else  
-				loginView::show($user);
-		} else  // Initial link
-			loginView::show(null);
-	}*/
-		    
-	    public static function run() {
-	    	$user = null;
-	    	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	    		$user = new User($_POST);
-	    		$users = UsersDB::getUsersBy('username', $user->getUserName());
-	    		if (empty($users))
-	    			$user->setError('username', 'USER_NAME_DOES_NOT_EXIST');
-	    		else {
-	    			if (strcmp($user->getPassword(), $users[0]->getPassword()) != 0)
-	    				$user->setError('password', 'PASSWORD_INCORRECT');
-	    			else
-	    				$user = $users[0];
-	    		}
-	    
-	    	}
-	    	$_SESSION['user'] = $user;
-	    	if (is_null($user) || $user->getErrorCount() != 0) {
-	    		userviewController::run();
-	    	} else {
-	    		homeView::show();
-	    		//header('Location: /'.$_SESSION['base']);
-	    	}
-	    }
+			$usertest = UsersDB::getUserBy('username', $_POST['userName']);
+				
+			if (is_null($usertest)){
+				echo "User not in database";
+				HomeView::show();
+			}else{
+				$user = $usertest;
+				$_SESSION['user'] = $user;
+				userviewController::run();
+				print_r($_SESSION['user']->getErrors());
+			}
+		}
+
+	}
 }
+
 ?>
