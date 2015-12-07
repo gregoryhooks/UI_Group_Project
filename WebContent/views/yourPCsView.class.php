542a31afc67f387fdcbbc9e9f872fa87f5d151f5
bjv500 <eapplonie@hotmail.com>
2015-12-07 12:42:55 -0600

    pcview ready

--- WebContent/views/yourPCsView.class.php
+++ WebContent/views/yourPCsView.class.php
@@ -1,12 +1,14 @@
 <?php  
 class yourPCsView {
 	public static function show() {
-
+		MasterView::showHeader2();
+		MasterView::showUsernavbar();
 		$usersaves = UserSavesDB::getUserSavesBy('userId', $_SESSION['user']);
+		MasterView::showCategories();
 		foreach($usersaves as $build){
 			MasterView::showItem($build);
 		}
-		
+		MasterView::showFooter();
 	}
 }
 ?>
